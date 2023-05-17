3bc63474e7f953e235d7956955c7a9fa4a5090f6
hxer520 <HXER520@GMAIL.COM>
2023-05-18 05:49:28 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/【丰团团】资源共享'],
+  protectedRoutes: ['/【丰团团】资源共享/资料'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:ringo@fengxtuan.com',
